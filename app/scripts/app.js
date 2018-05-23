e529322c75a89ef8855789b19d33044e29a95952
Shunsuke Murakami <murakami@chatwork.com>
2018-05-23 18:16:18 +0900

    use web site event to show member list

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,22 @@
 'use strict'
 
-const toButton = document.querySelector('#_to')
-const textarea = document.querySelector('#_chatText')
+const textareaSelector = '#_chatText'
 
 class App {
     constructor() {
+        this.toButton = document.querySelector('#_to')
     }
 
     /**
      *
+     * @param {string} text
      * @param {KeyboardEvent} event
      */
-    keyup(event) {
+    keypress(text, event) {
         const key = event.key
-        if (key === '@' && this.showMembers(this.textarea.value, key)) {
-            if (hasMember) {
-                toButton.click()
+        if (key === '@' && this.showMembers(text, key)) {
+            if (this.hasMember()) {
+                this.toButton.click()
                 // ignore '@'
                 // @ key is just flag to show menmber list
                 event.preventDefault()
@@ -27,7 +28,7 @@ class App {
      * @return {boolean}
      */
     hasMember() {
-        return toButton.style.display !== 'none'
+        return this.toButton.style.display !== 'none'
     }
 
     /**
@@ -36,15 +37,47 @@ class App {
      */
     showMembers(text, key) {
         // if just first chara or space or breakline before '@'
-        if (text === '@') {
+        if (text === '') {
             return true
         }
-        return text.substr(-2, 2).match(/\s@/)
+        return text.substr(-1, 1).match(/\s/)
     }
 }
 
-const app = new App()
+// make it to different file if this proj supports import/export build system i.e. webpack or browserify
+// from https://gist.github.com/jwilson8767/db379026efcbd932f64382db4b02853e
 
-textarea.addEventListener('keyup', (e) => {
-    app.keyup(e)
-})
+/**
+ * Waits for an element satisfying selector to exist, then resolves promise with the element.
+ * Useful for resolving race conditions.
+ *
+ * @param selector
+ * @returns {Promise}
+ */
+function elementReady(selector) {
+  return new Promise((resolve, reject) => {
+    let el = document.querySelector(selector);
+    if (el) {resolve(el);}
+    new MutationObserver((mutationRecords, observer) => {
+      // Query for elements matching the specified selector
+      Array.from(document.querySelectorAll(selector)).forEach((element) => {
+        resolve(element);
+        //Once we have resolved we don't need the observer anymore.
+        observer.disconnect();
+      });
+    })
+      .observe(document.documentElement, {
+        childList: true,
+        subtree: true
+      });
+  });
+}
+
+elementReady(textareaSelector)
+    .then((textarea) => {
+        const app = new App()
+        textarea.addEventListener('keypress', (e) => {
+            app.keypress(textarea.value, e)
+        })
+    })
+
