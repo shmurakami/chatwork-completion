6e95a87e9efc0220c7986bb36d5c9091e869737e
shaka <shmurakami1104@gmail.com>
2019-12-02 14:20:06 +0900

    fav repository

--- app/scripts/favorite.js
+++ app/scripts/favorite.js
@@ -2,15 +2,22 @@ import {elementReady} from "./element_ready";
 
 import starHeader from '../images/star_header.png'
 
-const parentSelector = '#_adminNavi'
+const headerParentSelector = '#_adminNavi'
+const sidebarParentSelector = '#_'
 
 class Favorite {
     constructor() {
+        this.favoriteItems = new FavoriteItems()
     }
 
     addMenu() {
-        const parent = document.querySelector(parentSelector)
+        const parent = document.querySelector(headerParentSelector)
         const button = this.createButtonElement()
+
+        button.addEventListener('click', () => {
+            this.clickListener()
+        })
+
         parent.appendChild(button)
     }
 
@@ -35,10 +42,54 @@ class Favorite {
 
         return list
     }
+
+    clickListener() {
+        console.log(this.favoriteItems.getList())
+    }
+
+    addSidebar() {
+
+    }
+
+    createSidebarElement() {
+
+    }
 }
 
-elementReady(parentSelector)
+class FavoriteItems {
+    constructor() {
+        this.storageKey = `chatworkCompletionFavorite`
+
+        const favorites = this.getFromStorage() || {items: []}
+        this.favorites = favorites.items
+    }
+
+    getList() {
+        return this.favorites
+    }
+
+    set(id, message) {
+        this.favorites[id] = message
+        this.setToStorage(this.favorites)
+    }
+
+    remove(id) {
+        delete this.favorites[id]
+        this.setToStorage(this.favorites)
+    }
+
+    setToStorage(favorites) {
+        localStorage.setItem(this.storageKey, JSON.stringify({items: favorites}))
+    }
+
+    getFromStorage() {
+        return localStorage.getItem(this.storageKey)
+    }
+}
+
+elementReady(headerParentSelector)
     .then(() => {
         const favorite = new Favorite()
         favorite.addMenu()
+        favorite.addSidebar()
     })
